31fe1714e2d0c71f4ac927a1f0d3831f5da4ea55
Oliver Roick <oliver.roick@gmail.com>
2012-11-22 20:57:36 +0100

    Now with real private conversion methods

--- Geojson2Wkt.js
+++ Geojson2Wkt.js
@@ -13,72 +13,72 @@ var Geojson2Wkt = (function(){
 	 * Constructor
 	 */
 	function geojson2Wkt() {
-		if (!instance) instance = this;
+		
+	}
 
-		/*
-		 * Returns string containing the coordinates of a point geometry
-		 */
-		this.parsePointGeometry = function(coordinates) {
-			return [coordinates[0], coordinates[1]].join(' ');
+	/*
+	 * Returns string containing the coordinates of a point geometry
+	 */
+	var parsePointGeometry = function(coordinates) {
+		return [coordinates[0], coordinates[1]].join(' ');
+	}
+
+	/*
+	 * 
+	 */
+	var parseMultiPointGeometry = function(coordinates) {
+		var multipointWkt = [];
+		for (var i = 0; i < coordinates.length; i++) {
+			multipointWkt.push(parsePointGeometry(coordinates[i]));
+		}
+		return multipointWkt.join(', ');
+	}
+
+	/*
+	 * Returns string containing the coordinates of a line geometry. Breaks coordinates array into single 
+	 * latitude-/longitude pairs and uses parsePointGeometry to parse node.
+	 */
+	var parseLineGeometry = function(coordinates) {
+		var lineWkt = [];
+		for (var i = 0; i < coordinates.length; i++) {
+			lineWkt.push(parsePointGeometry(coordinates[i]));
+		}
+		return lineWkt.join(', ');
+	}
+
+	/*
+	 *
+	 */
+	var parseMultiLineGeometry = function(coordinates) {
+		var multiLineWkt = [];
+		for (var i = 0; i < coordinates.length; i++) {
+			multiLineWkt.push('(' + parseLineGeometry(coordinates[i]) + ')');
 		}
 
-		/*
-		 * 
-		 */
-		this.parseMultiPointGeometry = function(coordinates) {
-			var multipointWkt = [];
-			for (var i = 0; i < coordinates.length; i++) {
-				multipointWkt.push(this.parsePointGeometry(coordinates[i]));
-			}
-			return multipointWkt.join(', ');
-		}
+		return multiLineWkt.join(', ');
+	}
 
-		/*
-		 * Returns string containing the coordinates of a line geometry. Breaks coordinates array into single 
-		 * latitude-/longitude pairs and uses parsePointGeometry to parse node.
-		 */
-		this.parseLineGeometry = function(coordinates) {
-			var lineWkt = [];
-			for (var i = 0; i < coordinates.length; i++) {
-				lineWkt.push(this.parsePointGeometry(coordinates[i]));
-			}
-			return lineWkt.join(', ');
+	/*
+	 * Returns string containing the coordinates of a polygon geometry. Breaks linstring array into single 
+	 * line segment arrays and uses parseLineGeometry to parse line segments.
+	 */
+	var parsePolygonGeometry = function(coordinates) {
+		var polygonWkt = [];
+		for (var i = 0; i < coordinates.length; i++) {
+			polygonWkt.push('(' + parseLineGeometry(coordinates[i]) + ')');
 		}
+		return polygonWkt.join(', ');
+	}
 
-		/*
-		 *
-		 */
-		this.parseMultiLineGeometry = function(coordinates) {
-			var multiLineWkt = [];
-			for (var i = 0; i < coordinates.length; i++) {
-				multiLineWkt.push('(' + this.parseLineGeometry(coordinates[i]) + ')');
-			}
-
-			return multiLineWkt.join(', ');
+	/*
+	 *
+	 */
+	var parseMultiPolygonGeometry = function(coordinates) {
+		var multiPolygonWkt = [];
+		for (var i = 0; i < coordinates.length; i++) {
+			multiPolygonWkt.push('(' + parsePolygonGeometry(coordinates[i]) + ')');
 		}
-
-		/*
-		 * Returns string containing the coordinates of a polygon geometry. Breaks linstring array into single 
-		 * line segment arrays and uses parseLineGeometry to parse line segments.
-		 */
-		this.parsePolygonGeometry = function(coordinates) {
-			var polygonWkt = [];
-			for (var i = 0; i < coordinates.length; i++) {
-				polygonWkt.push('(' + this.parseLineGeometry(coordinates[i]) + ')');
-			}
-			return polygonWkt.join(', ');
-		}
-
-		/*
-		 *
-		 */
-		this.parseMultiPolygonGeometry = function(coordinates) {
-			var multiPolygonWkt = [];
-			for (var i = 0; i < coordinates.length; i++) {
-				multiPolygonWkt.push('(' + this.parsePolygonGeometry(coordinates[i]) + ')');
-			}
-			return multiPolygonWkt.join(', ');
-		}
+		return multiPolygonWkt.join(', ');
 	}
 
 	/*
@@ -98,22 +98,22 @@ var Geojson2Wkt = (function(){
 
 		switch (json.type) {
 			case 'Point': 
-				wkt = this.parsePointGeometry(coordinates);
+				wkt = parsePointGeometry(coordinates);
 				break;
 			case 'MultiPoint': 
-				wkt = this.parseMultiPointGeometry(coordinates);
+				wkt = parseMultiPointGeometry(coordinates);
 				break;
 			case 'LineString': 
-				wkt = this.parseLineGeometry(coordinates);
+				wkt = parseLineGeometry(coordinates);
 				break;
 			case 'MultiLineString': 
-				wkt = this.parseMultiLineGeometry(coordinates);
+				wkt = parseMultiLineGeometry(coordinates);
 				break;
 			case 'Polygon': 
-				wkt = this.parsePolygonGeometry(coordinates);
+				wkt = parsePolygonGeometry(coordinates);
 				break;
 			case 'MultiPolygon': 
-				wkt = this.parseMultiPolygonGeometry(coordinates);
+				wkt = parseMultiPolygonGeometry(coordinates);
 				break;
 			default: 
 				throw new Error('Not able to parse geometry. Property type not set in GeoJSON object.');
@@ -121,7 +121,12 @@ var Geojson2Wkt = (function(){
 		return [json.type.toUpperCase(), '(', wkt, ')'].join('');
 	}
 
-	return new geojson2Wkt();
+	var createInstance = function () {
+		if (!instance) instance = new geojson2Wkt();
+		return instance;
+	}
+
+	return new createInstance();
 })();
 
 module.exports = Geojson2Wkt;
